062baf0acc42a3c3547207d5d675ce3bcada127d
Christine Manegan <christineastoria@gmail.com>
2025-10-02 12:56:55 -0400

    fix apikeys

--- src/mooch/firebase/firebaseConfig.js
+++ src/mooch/firebase/firebaseConfig.js
@@ -1,24 +1,18 @@
 // Import the functions you need from the SDKs you need
-// import * as firebase from 'firebase';
 import { initializeApp } from 'firebase/app';
 import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
 import { getFirestore, collection, doc, addDoc, setDoc , updateDoc, arrayUnion, onSnapshot, getDocs , getDoc, arrayRemove, getCountFromServer, deleteDoc} from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL, getMetadata, listAll } from "firebase/storage";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCNY3QtX3Rcc2q4uL4BZJI3seiMHkW6aUI",
-  authDomain: "mooch-65f87.firebaseapp.com",
-  projectId: "mooch-65f87",
-  storageBucket: "mooch-65f87.appspot.com",
-  messagingSenderId: "112146305733",
-  appId: "1:112146305733:web:699568a71dbd72ded416ac",
-  measurementId: "G-N0417L2WLJ",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 // storageBucket: "gs://mooch-65f87.appspot.com"
 // Initialize Firebase
